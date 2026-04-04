5decbc9936d223d905028470c345e75c75a2385b
tomy2407 <elmaanasoufiane664@gmail.com>
2026-04-04 19:03:10 +0100

    Fix port Gmail 587

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,9 @@ const JWT_SECRET = process.env.JWT_SECRET || 'cvgen-secret-2024';
 
 // ── Transporteur Email ────────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
